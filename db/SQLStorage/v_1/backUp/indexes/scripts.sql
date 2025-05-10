858cfc19cf92a90ceeb9c2f275b4ca1b635d76b4
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-05-10 13:31:45 +0300

    Удалил старую версию

--- db/SQLStorage/v_1/backUp/indexes/scripts.sql
+++
@@ -1,2 +0,0 @@
-create index idx_user_login on users (login);
-create index idx_messages_sent_at on messages (sent_at);
--- include/PlatformOSInfo/PlatformOSInfo.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-std::string getOSInfo();
-std::string getProcessInfo();
-#if defined(_WIN32)
-void initConsoleOutput();
-#endif
