36e17c9760b7fd27ea5d8c1603865e4fc859b1f1
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-05-15 13:29:50 +0300

    Обновление файлов cpp

--- root/server/src/ClientHandler.cpp
+++ root/server/src/ClientHandler.cpp
@@ -1,6 +1,5 @@
 #include "ClientHandler.h"
 #include "Logger.h"
-
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -8,44 +7,83 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #endif
-
 #include <cstring>
-
+#include <string>
+#include <algorithm>
 ClientHandler::ClientHandler(int socket)
     : clientSocket(socket),
       handlerThread(&ClientHandler::run, this)
 {
     handlerThread.detach();
 }
-
 ClientHandler::~ClientHandler()
 {
-}
-
-void ClientHandler::run()
-{
-    char buffer[1024];
-    Logger::getInstance().log("Client connected: socket=" + std::to_string(clientSocket));
-
-    while (true)
-    {
-        std::memset(buffer, 0, sizeof(buffer));
-        int bytes = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytes <= 0)
-        {
-            Logger::getInstance().log("Client disconnected: socket=" + std::to_string(clientSocket));
-            break;
-        }
-
-        std::string msg(buffer, bytes);
-        Logger::getInstance().log("Received from " + std::to_string(clientSocket) + ": " + msg);
-
-        send(clientSocket, buffer, bytes, 0);
-    }
-
 #ifdef _WIN32
     closesocket(clientSocket);
 #else
     close(clientSocket);
 #endif
+}
+void ClientHandler::run()
+{
+    char buffer[1024];
+    bool loggedIn = false;
+    std::string username;
+    Logger::getInstance().log("Клиент подключён: socket=" + std::to_string(clientSocket));
+    while (true)
+    {
+        std::memset(buffer, 0, sizeof(buffer));
+        int bytes = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (bytes <= 0)
+        {
+            Logger::getInstance().log("Клиент отключён: socket=" + std::to_string(clientSocket));
+            break;
+        }
+        std::string msg(buffer, bytes);
+        Logger::getInstance().log("Получено от " + std::to_string(clientSocket) + ": " + msg);
+        if (!msg.empty() && msg.back() == '\n')
+            msg.pop_back();
+        if (!loggedIn)
+        {
+            const std::string prefix = "LOGIN ";
+            if (msg.rfind(prefix, 0) == 0)
+            {
+                auto spacePos = msg.find(' ', prefix.size());
+                if (spacePos != std::string::npos)
+                {
+                    username = msg.substr(prefix.size(), spacePos - prefix.size());
+                }
+                else
+                {
+                    username = msg.substr(prefix.size());
+                }
+                const char *ok = "LOGIN_OK\n";
+                send(clientSocket, ok, strlen(ok), 0);
+                Logger::getInstance().log("Пользователь вошёл: " + username);
+                loggedIn = true;
+                continue;
+            }
+            else
+            {
+                const char *err = "LOGIN_ERROR\n";
+                send(clientSocket, err, strlen(err), 0);
+                continue;
+            }
+        }
+        else
+        {
+            const std::string mtx = "MESSAGE ";
+            if (msg.rfind(mtx, 0) == 0)
+            {
+                std::string echo = "MESSAGE_OK " + msg.substr(mtx.size()) + "\n";
+                send(clientSocket, echo.c_str(), echo.size(), 0);
+                Logger::getInstance().log("Эхо для " + username + ": " + echo);
+            }
+            else
+            {
+                const char *err = "UNKNOWN_CMD\n";
+                send(clientSocket, err, strlen(err), 0);
+            }
+        }
+    }
 }
